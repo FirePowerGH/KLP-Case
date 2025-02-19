5b6c584c64d17b277f5a777b3b83d618a5bd3e2a
FirePower <31512975+FirePowerGH@users.noreply.github.com>
2025-02-19 23:09:48 +0100

    Create Database and Frontpage

---
+++ backend/api.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+def createTable():
+    # Koble til databasen, eller lage en hvis den ikke finnes
+    conn = sqlite3.connect('backend/database.db')
+
+    # Lage en cursor for å utføre SQL-spørringer
+    cursor = conn.cursor()
+
+    # Lage en tabell for bruker informasjon
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS brukere (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            navn VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            tlf INT NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            fødselnummer INT NOT NULL,
+            kontonr INT NOT NULL
+        )'''
+    )
+
+    # Lage en tabell for transaksjoner
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS transaksjon (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            brukerid VARCHAR(255) NOT NULL
+        )'''
+    )
+
+    conn.commit()
+    cursor.close()
+    conn.close()
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
+from backend.api import *
+
+app = Flask(__name__, template_folder='static/templates')
+
+@app.route('/')
+def index():
+    createTable()
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) 
